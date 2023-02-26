6a7d9d3130cd108eab58f8faa1e7e1ffcf6126f1
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2023-02-26 06:16:43 -0800

    5-printf.c fix_2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 
+/**
+ * main-Hello, printf
+ * description: using printf to print text
+ * return : 0
+ **/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
