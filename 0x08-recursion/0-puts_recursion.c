139c4554ddc98e5011376c4320864fde626ae1a8
dennis william <dennisanietimfonwilliam005@gmail.com>
2023-06-09 12:31:57 -0700

    recursion project

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * _puts_recursion - a standard library that provides a similar function as puts( ) 
+ *@s: character input
+ *Return: Always 0
+ *Description: a standard library that provides a similar functuion as puts() followed by a new line.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+		_putchar(*s);
+		_puts_recursion(s + 1);
+}
