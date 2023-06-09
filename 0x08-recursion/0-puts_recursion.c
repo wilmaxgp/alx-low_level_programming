5bde5fc442a5857736c0fc6d831e9c4aa1d80aaf
dennis william <dennisanietimfonwilliam005@gmail.com>
2023-06-09 12:44:03 -0700

    recursion task 1 update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
-/*
- * _puts_recursion - a standard library that provides a similar function as puts( ) 
+/**
+ *_puts_recursion - a standard library that provides a similar function as puts( ) 
  *@s: character input
- *Return: Always 0
+ *Return: Always void
  *Description: a standard library that provides a similar functuion as puts() followed by a new line.
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+
 	{
 		_putchar('\n');
+		return;
 	}
-		_putchar(*s);
-		_puts_recursion(s + 1);
+
+		_putchar (*s);
+		_puts_recursion (s + 1);
+
 }
