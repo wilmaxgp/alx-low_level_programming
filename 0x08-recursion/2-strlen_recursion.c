bfffcf5b48533713455ca6e3d22dc966efee3ec5
dennis william <dennis william>
2023-06-10 04:43:10 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * strlen_recursion - function that returns the length of string 
+ * @*s: string input
+ * Description: function that returns the length of string
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0' )
+	{
+		return(0);
+	}
+	else
+	{
+		return(1 + _strlen_recursion(s + 1));
+	}
+}
+
