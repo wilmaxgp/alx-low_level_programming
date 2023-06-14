70dfaf083ffd2088b38384cd53ff20f199294acf
dennis william <dennis william>
2023-06-13 18:09:10 -0700

    Task 5 betty update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,9 +2,9 @@
 #include <math.h>
 
 /**
- * _pow_recursion: function that returns the value x raised to the power of y.
- * @int x: integer of x
- * @int y: integer of y
+ * _pow_recursion:-function that returns the value x raised to the power of y.
+ * @x: integer of x
+ * @y: integer of y
  * Description: function returns the value of x raised to the power of y.
  * Return: Always int.
  **/
@@ -13,13 +13,13 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
-		return(-1);
+		return (-1);
 	}
 	else if (y == 0)
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
