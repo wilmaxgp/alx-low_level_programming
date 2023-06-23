e69d5570464659bd4499e159cbef0482e541ef39
dennis william <dennis william>
2023-06-23 12:37:54 -0700

    updated print numbers, followed by new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,20 +13,20 @@
  */
 void print_numbers(const char* separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    int num;
+       	va_list args;
+    	unsigned int i;
+    	int num;
 
-    va_start(args, n);
+    	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        num = va_arg(args, int);
-        printf("%d", num);
-
-        if (separator != NULL && i < n - 1)
-            printf("%s", separator);
-    }
+    	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+		
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
 
     va_end(args);
 
