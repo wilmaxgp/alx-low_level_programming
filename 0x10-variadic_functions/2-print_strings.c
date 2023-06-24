6cdc31a60473840817dc74c42e7951b25a1e3b3a
dennis william <dennis william>
2023-06-24 08:05:47 -0700

    Betty correection on function that prints strings floowed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
-		
+
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
