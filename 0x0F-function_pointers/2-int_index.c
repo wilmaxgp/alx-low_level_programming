293d99662ac22a41edae75e4628e6c6c0f9b859f
dennis william <dennis william>
2023-06-24 08:46:04 -0700

    function that searches integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,12 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array && cmp && size > 0)
-		
+
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
-				return i;
+				return(i);
 		}
 
-	return -1;
+	return (-1);
 }
