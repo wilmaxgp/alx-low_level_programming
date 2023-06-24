ca36a7c8b209391cf58771454d03902a71dd9fdf
dennis william <dennis william>
2023-06-24 07:53:59 -0700

    update betty error on function that prints anything

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,19 +16,19 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 	int num;
-	
+
 	va_start(args, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
 		printf("%d", num);
-		
+
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
-	
+
 	va_end(args);
-	
+
 	printf("\n");
 }
