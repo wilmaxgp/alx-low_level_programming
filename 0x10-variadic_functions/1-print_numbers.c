d2bb9377cdf34d4a690611262ae85c0cb624d180
dennis william <dennis william>
2023-06-24 07:45:01 -0700

    function that prints anything

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,15 +11,15 @@
  *
  * Return: void
  */
-void print_numbers(const char* separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-       	va_list args;
-    	unsigned int i;
-    	int num;
-
-    	va_start(args, n);
-
-    	for (i = 0; i < n; i++)
+	va_list args;
+	unsigned int i;
+	int num;
+	
+	va_start(args, n);
+	
+	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
 		printf("%d", num);
@@ -27,8 +27,8 @@ void print_numbers(const char* separator, const unsigned int n, ...)
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
-
-    va_end(args);
-
-    printf("\n");
+	
+	va_end(args);
+	
+	printf("\n");
 }
