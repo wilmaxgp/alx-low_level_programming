0d88b225b1573c392c3ed2958b2d278b164556bc
dennis william <dennis william>
2023-06-25 16:33:54 -0700

    function that prints opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_opcodes -  prints the opcodes of its own main function
+ * main -  prints the opcodes of its own main function
  *
  * @argc: The number of arguments passed to the program
  * @argv: An array of pointers to the arguments
- * @num_bytes: specifies the number of bytes of the 
  * main function's opcodes to be printed
+ * Return: main
  */
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
 	int num_bytes, i;
 	char *address;
