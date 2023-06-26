41c60c2fbb999f3011da64290bcc75f602a38e1b
dennis william <dennis william>
2023-06-25 17:18:58 -0700

    define new type structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,12 +3,17 @@
 
 #include <stdio.h>
 
-
+/**
+ * struct dog - Represents a dog with its name, age, and owner.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+} dog;
 
 #endif
