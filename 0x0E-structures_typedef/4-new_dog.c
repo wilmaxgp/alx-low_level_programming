8713ca02703b7e9624285d61b423396c6920ca36
dennis william <dennis william>
2023-06-25 21:06:07 -0700

    function that frees dogs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,7 +7,7 @@
  * @age: age
  * @owner: owner
  *
- * Return : new_dog
+ * Return: new_dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -17,8 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 	{
 		return (NULL);
-	}
-	
+	}	
 
 
 	new_dog->name = malloc(strlen(name) + 1);
