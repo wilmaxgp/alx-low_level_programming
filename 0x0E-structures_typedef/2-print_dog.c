b98b9b3a5b43abeb836562092cd62336b1c8f330
dennis william <dennis william>
2023-06-25 20:31:57 -0700

    print a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * Description: function that prints a struct dog
+ * @d: intialize variable
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+		
+
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
-
 #include <stdio.h>
 
 /**
- * struct dog - Represents a dog with its name, age, and owner.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - Entry point
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: Define struct dog with the three members
  */
-typedef struct dog
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
