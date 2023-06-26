badab05114c27b20392be8c30c654b80a3336b3c
dennis william <dennis william>
2023-06-26 06:08:21 -0700

    function that compares two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,11 +15,10 @@ int wildcmp(char *s1, char *s2)
 		return (1);	
 
 	if (*s1 == *s2 || *s2 == '*')
-		return (wildcmp(s1 + 1, s2 + 1));
-	
-	if (*s2 == '*')
-		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
-			return (1);
-	return 0;
-}
+	{	if (*s2 == '*' && wildcmp(s1, s2 + 1))
+			return 1;
+		return wildcmp(s1 + 1, s2 + 1);
+	}
 
+    return 0;
+}
