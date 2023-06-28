8fbb4c5130f1a1615acaa8a7b4e8bfa9055c999b
dennis william <dennis william>
2023-06-28 02:08:29 -0700

    wildcmp function update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,7 +12,7 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	
+
 	if (*s2 == '*')
 	{
 		if (*s1 == '\0' && *(s2 + 1) == '\0')
@@ -22,10 +22,10 @@ int wildcmp(char *s1, char *s2)
 		if (wildcmp(s1, s2 + 1) == 1)
 			return (1);
 	}
-	
+
 	if (*s1 == *s2 && *s1 != '\0')
 		return (wildcmp(s1 + 1, s2 + 1));
-	
+
 	return (0);
 }
 
