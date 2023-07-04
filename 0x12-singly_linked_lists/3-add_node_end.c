2f5df6aa6c648a2e6c53ea98ec415b860c89f81f
dennis william <dennis william>
2023-07-04 10:47:26 -0700

    function that adds a new node at the end of a list_t list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,14 +10,17 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
+	list_t *new_node, *last_node;
+	size_t len = 0;
+
+
 	if (str == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-       
+
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
@@ -25,20 +28,29 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
+
+	while (str[len])
+		len++;
+
+
+	new_node->len = len;
 	new_node->next = NULL;
+
 
 	if (*head == NULL)
 	{
 		*head = new_node;
+		return (new_node);
 	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new_node;
-	}
+
+
+	last_node = *head;
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+
+
+	last_node->next = new_node;
+
 
 	return (new_node);
 }
-
