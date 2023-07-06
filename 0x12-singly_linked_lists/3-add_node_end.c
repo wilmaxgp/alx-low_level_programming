4a72967d2e9970465f62a3c5a978e6c66d7ce1aa
dennis william <dennis william>
2023-07-06 07:29:42 -0700

    function that frees a list_t list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,7 +3,7 @@
 /**
  * add_node_end - function that adds a new node at the end of a list_t list
  * @str: pointer to a constant character string to be stored in node
- * @head: pointer to a pointer to the head of the list 
+ * @head: pointer to a pointer to the head of the list
  *
  * Description: function that adds a new node at the end of a list_t list
  * Return: the address of the new element, or NULL if it failed
@@ -12,7 +12,6 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *last_node;
 	size_t len = 0;
-
 
 	if (str == NULL)
 		return (NULL);
@@ -28,14 +27,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-
 	while (str[len])
 		len++;
-
-
 	new_node->len = len;
 	new_node->next = NULL;
-
 
 	if (*head == NULL)
 	{
@@ -43,14 +38,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (new_node);
 	}
 
-
 	last_node = *head;
 	while (last_node->next != NULL)
 		last_node = last_node->next;
 
-
 	last_node->next = new_node;
-
-
 	return (new_node);
 }
