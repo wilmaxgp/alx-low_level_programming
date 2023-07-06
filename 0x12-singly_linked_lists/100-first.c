e2377bc22ee8201d85bd825b4ea10a71dfe21549
dennis william <dennis william>
2023-07-06 08:03:33 -0700

    function that print updates befor main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,11 +2,12 @@
 
 /**
  * first_message - function that prints first_message befor printing main
- * 
  * Description: function that prints first_message befor printing main
+ *
  * Return: void
  */
 void __attribute__((constructor)) first_message(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
