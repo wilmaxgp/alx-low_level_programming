bb94d9a2c41647c297098de358d4a0ee2dce23e3
dennis william <dennis william>
2023-07-06 07:44:14 -0700

    function that adds a new node at the end of a list_t list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,8 @@
 #include "lists.h"
 
+list_t *create_node(const char *str);
+list_t *find_last_node(list_t *head);
+
 /**
  * add_node_end - function that adds a new node at the end of a list_t list
  * @str: pointer to a constant character string to be stored in node
@@ -11,15 +14,47 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *last_node;
-	size_t len = 0;
 
 	if (str == NULL)
 		return (NULL);
 
+	new_node = create_node(str);
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	last_node = find_last_node(*head);
+	if (last_node == NULL)
+	{
+		free(new_node->str);
+		free(new_node);
+		return (NULL);
+	}
+
+	last_node->next = new_node;
+
+	return (new_node);
+}
+
+/**
+ * create_node - Creates a new node and duplicates the string.
+ * @str: The string to be duplicated and stored in the new node.
+ *
+ * Return: The address of the new node, or NULL if it failed.
+ */
+list_t *create_node(const char *str)
+{
+	list_t *new_node;
+	size_t len = strlen(str);
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-
+	
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
@@ -27,21 +62,24 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	while (str[len])
-		len++;
 	new_node->len = len;
 	new_node->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-
-	last_node = *head;
-	while (last_node->next != NULL)
-		last_node = last_node->next;
-
-	last_node->next = new_node;
+	
 	return (new_node);
+}
+
+/**
+ * find_last_node - Finds the last node in the list.
+ * @head: The head of the list.
+ *
+ * Return: The address of the last node, or NULL if the list is empty.
+ */
+list_t *find_last_node(list_t *head)
+{
+	list_t *current = head;
+	
+	while (current->next != NULL)
+		current = current->next;
+
+	return (current);
 }
