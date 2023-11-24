5dccc78f04af106a5a86a80ed01b9cb4bac00d7a
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2023-11-24 01:04:42 -0800

    fix trailing whites spaces errors in function set bit function file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,14 +9,14 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
-	
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-	
+
 	mask = 1UL << index;
 	*n = *n | mask;
-	
+
 	return (1);
 }
