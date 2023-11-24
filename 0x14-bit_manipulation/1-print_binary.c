816e90dd6f37611ed4d82e5ed7c7cd9044bfac3d
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2023-11-24 00:42:35 -0800

    print binary function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,24 +9,24 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	int flag = 0;
+    unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+    int flag = 0;
 
-	while (mask > 0)
-	{
-		if ((n & mask) || flag)
-		{
-			flag = 1;
-			if (n & mask)
-				_putchar('1');
-			else
-				_putchar('0');
-		}
-		mask >>= 1;
-	}
+    while (mask > 0)
+    {
+        if ((n & mask) || flag)
+        {
+            flag = 1;
+            if (n & mask)
+                _putchar('1');
+            else
+                _putchar('0');
+        }
+        mask >>= 1;
+    }
 
-	if (!flag)
-		_putchar('0');
+    if (!flag)
+        _putchar('0');
 
-	_putchar('\n');
+    _putchar('\n');
 }
