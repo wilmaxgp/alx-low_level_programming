56ed24fb07037a45a52a2904bfcbe2be7f598eef
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2023-11-24 00:38:25 -0800

    function that prints binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,14 +4,14 @@
  * print_binary - Prints the binary representation of a number
  * @n: The number to print in binary
  *
- * Description: This function prints the binary representation of a given number
- * without using arrays, malloc, %, or / operators.
+ * Description: This function prints the binary representation of a given
+ * number without using arrays, malloc, %, or / operators.
  */
 void print_binary(unsigned long int n)
 {
 	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 	int flag = 0;
-	
+
 	while (mask > 0)
 	{
 		if ((n & mask) || flag)
@@ -24,9 +24,9 @@ void print_binary(unsigned long int n)
 		}
 		mask >>= 1;
 	}
-	
+
 	if (!flag)
 		_putchar('0');
-	
+
 	_putchar('\n');
 }
