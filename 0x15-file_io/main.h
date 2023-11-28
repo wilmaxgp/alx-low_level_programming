429c5be97fb8d9e915ddbc37dff29c6891390a22
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2023-11-27 17:48:48 -0800

    Task 4 fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,5 +14,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_exit(int err_code, char *msg);
+int main(int argc, char *argv[]);
+void print_elf_info(Elf32_Ehdr *header) 	 	
 
 #endif
