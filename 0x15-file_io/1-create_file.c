d3d1aec1d86251190b961b3668b17b4c293952de
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2023-11-27 17:33:44 -0800

    task 2 fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,11 +15,11 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes_written;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (fd == -1)
-		return -1;
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -30,10 +30,10 @@ int create_file(const char *filename, char *text_content)
 		if (bytes_written != len)
 		{
 			close(fd);
-			return -1;
+			return (-1);
 		}
 	}
 
 	close(fd);
-	return 1;
+	return (1);
 }
