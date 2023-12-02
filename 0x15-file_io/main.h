187c24bb49f741b4df690d19f939da0d9313bf29
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2023-12-02 15:24:44 -0800

    update code

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,9 +10,13 @@
 #include <elf.h>
 
 #define BUF_SIZE 1024
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_exit(int err_code, const char *msg);
+void close_file(int fd, const char *filename);
+int copy_file(int argc, char *argv[]);
 
 #endif
