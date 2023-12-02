78f666bf6322db844bb7a36e6948ae59cea01c75
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2023-12-02 15:46:21 -0800

    update code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -38,6 +38,48 @@ void close_file(int fd, const char *filename)
 }
 
 /**
+ * open_file - Open a file with specified mode
+ * @filename: Name of the file to open
+ * @mode: Mode for opening the file
+ * Return: File descriptor on success, -1 on failure
+ *
+ * This function attempts to open a file with the specified mode.
+ */
+int open_file(const char *filename, int mode)
+{
+	int fd = open(filename, mode);
+
+	return (fd);
+}
+
+/**
+ * copy_file_contents - Copy contents from one file to another
+ * @fd_from: File descriptor to read from
+ * @fd_to: File descriptor to write to
+ *
+ * This function copies contents from one file descriptor to another.
+ */
+void copy_file_contents(int fd_from, int fd_to)
+{
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			error_exit(99, "Error: Can't write to file");
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		error_exit(98, "Error: Can't read from file");
+	}
+}
+
+/**
  * main - Entry point
  * @argc: Argument count
  * @argv: Argument vector
@@ -49,56 +91,28 @@ void close_file(int fd, const char *filename)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
+	{
 		error_exit(97, "Usage: cp file_from file_to");
-
-	fd_from = open(argv[1], O_RDONLY);
+	}
+	
+	fd_from = open_file(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		char error_msg[256];
-
-		snprintf(error_msg, sizeof(error_msg),
-				"Error: Can't read from file %s", argv[1]);
-		error_exit(98, error_msg);
+		error_exit(98, "Error: Can't read from file");
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (fd_to == -1)
 	{
-		char error_msg[256];
-		
-		snprintf(error_msg, sizeof(error_msg),
-				"Error: Can't write to %s", argv[2]);
-		error_exit(99, error_msg);
+		error_exit(99, "Error: Can't write to file");
 	}
 
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			char error_msg[256];
-
-			snprintf(error_msg, sizeof(error_msg),
-					"Error: Can't write to %s", argv[2]);
-			error_exit(99, error_msg);
-		}
-	}
-
-	if (bytes_read == -1)
-	{
-		char error_msg[256];
-
-		snprintf(error_msg, sizeof(error_msg),
-				"Error: Can't read from file %s", argv[1]);
-		error_exit(98, error_msg);
-	}
+	copy_file_contents(fd_from, fd_to);
 
 	close_file(fd_from, argv[1]);
 	close_file(fd_to, argv[2]);
 
-	return (0);
+	return (0;)
 }
