c6e0b6b17e37208c47080509f048454a703bdac3
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2023-12-02 19:02:43 -0800

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,12 +30,16 @@ void error_exit(int code, char *message, char *file_name, int fd) {
 void copy_file(const char *file_from, const char *file_to) {
     int fd_from, fd_to, bytes_read, bytes_written;
     char buffer[BUFFER_SIZE];
+    mode_t prev_umask;
 
     fd_from = open(file_from, O_RDONLY);
     if (fd_from == -1)
         error_exit(98, "Error: Can't read from file %s\n", (char *)file_from, -1);
 
+    prev_umask = umask(0);
     fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    umask(prev_umask);
+
     if (fd_to == -1)
         error_exit(99, "Error: Can't write to %s\n", (char *)file_to, fd_from);
     while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
