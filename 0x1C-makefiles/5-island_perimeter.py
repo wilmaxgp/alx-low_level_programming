516b9851bba52707f148ca5a721084512da3bd9a
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-28 14:47:34 -0700

    Implement island perimeter function in Python

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
 """
-5-main
+5-island_perimeter
 """
+
+
 def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in grid.
+
+    Args:
+        grid (list[list[int]]): A list of lists representing the grid.
+
+    Returns:
+        int: The perimeter of the island.
+
+    """
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
+
     return perimeter
+
 
 if __name__ == "__main__":
     grid = [
@@ -23,3 +40,4 @@ if __name__ == "__main__":
         [0, 0, 0, 0, 0, 0]
     ]
     print(island_perimeter(grid))
+
