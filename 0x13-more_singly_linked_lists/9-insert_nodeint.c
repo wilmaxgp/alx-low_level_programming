9446e9087426df74e4419bd61a96f57e9648cb1d
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-04 07:36:00 +0100

    fixed betty style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,40 +10,40 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new_node, *temp;
-    unsigned int i = 0;
+	listint_t *new_node, *temp;
+	unsigned int i = 0;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
+	new_node->n = n;
 
-    if (idx == 0)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    temp = *head;
-    while (temp != NULL && i < idx - 1)
-    {
-        temp = temp->next;
-        i++;
-    }
+	temp = *head;
+	while (temp != NULL && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
 
-    if (temp == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (temp == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->next = temp->next;
-    temp->next = new_node;
+	new_node->next = temp->next;
+	temp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
