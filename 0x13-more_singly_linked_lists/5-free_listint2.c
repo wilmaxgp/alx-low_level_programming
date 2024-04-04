059424e7340c65cd714bd0b37744357a7b669ea5
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-04 01:55:37 +0100

    created function that frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,16 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
-    listint_t *temp;
+	listint_t *temp;
 
-    if (head == NULL || *head == NULL)
-        printf("Freed !\n");
-        return;
+	if (head == NULL || *head == NULL)
+		printf("Freed !\n");
+	return;
 
-    while (*head != NULL)
-    {
-        temp = *head;
-        *head = (*head)->next;
-        free(temp);
-    }
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
 }
