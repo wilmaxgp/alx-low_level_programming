e925089378961c8ade5e1483c7dbcc4d0fcc94cb
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-04 02:36:29 +0100

    fixed betty style

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,15 +6,15 @@
  */
 void free_listint2(listint_t **head)
 {
-    listint_t *temp;
-    
-    if (head == NULL)
-        return;
+	listint_t *temp;
 
-    while (*head != NULL)
-    {
-        temp = *head;
-        *head = (*head)->next;
-        free(temp);
-    }
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
 }
