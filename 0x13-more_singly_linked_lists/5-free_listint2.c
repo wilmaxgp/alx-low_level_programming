1820c4c3dfcbc53d123081c06c41fa8b4b2d97d3
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-04 01:48:34 +0100

    fixed a free list error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,15 +6,12 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+    listint_t *temp;
 
-    if (head == NULL)
-        return;
-
-	while (*head != NULL)
-	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
-	}
+    while (*head != NULL)
+    {
+        temp = *head;
+        *head = (*head)->next;
+        free(temp);
+    }
 }
