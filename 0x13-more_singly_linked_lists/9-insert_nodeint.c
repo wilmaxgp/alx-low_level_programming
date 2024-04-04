6e8dacb54e21f69b5724d9c6efc9e0d24fad1cb2
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-04 07:31:44 +0100

    created function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
         i++;
     }
 
-    if (temp == NULL || temp->next == NULL)
+    if (temp == NULL)
     {
         free(new_node);
         return (NULL);
