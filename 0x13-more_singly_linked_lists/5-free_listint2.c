647133f5dc201cf426dda40aad0db7a3ba090f8a
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-04 02:19:20 +0100

    fixed a free list error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,32 +1,22 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees a listint_t list and sets the head to NULL
- * @head: A pointer to a pointer to the head of the list
- *
- * Description: This function frees a listint_t list and sets the head to NULL.
- *              It uses the listint_t struct defined in lists.h header file.
+ * free_listint2 - frees a listint_t list and sets the head to NULL
+ * @head: pointer to a pointer to the head of the list
  */
 void free_listint2(listint_t **head)
 {
     listint_t *temp;
-    listint_t *current = *head;
-    int count = 0;
 
-    if (head == NULL || *head == NULL)
-    {
-        printf("Freed !\n");
+    if (head == NULL)
         return;
-    }
 
-    while (current != NULL)
+    while (*head != NULL)
     {
-        temp = current;
-        current = current->next;
+        temp = *head;
+        *head = (*head)->next;
         free(temp);
-        count++;
     }
 
-    *head = NULL; /* Set the head to NULL after freeing the list */
-    printf("Freed %d elements\n", count);
+    printf("Freed !\n");
 }
