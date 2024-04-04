f8809a1bff69b65818c5176caabc2b06e1ed5bb5
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-04 09:32:10 +0100

    fixed execution success and other errors

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,37 +10,56 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow, *fast;
-	size_t count = 0;
+    const listint_t *slow, *fast;
+    size_t count = 0;
+    int loop_detected = 0;
 
-	if (head == NULL)
-		return (0);
+    if (head == NULL)
+        return (0);
 
-	slow = head;
-	fast = head;
+    slow = head;
+    fast = head;
 
-	while (slow != NULL && fast != NULL && fast->next != NULL)
-	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		slow = slow->next;
-		fast = fast->next->next;
+    while (slow != NULL && fast != NULL && fast->next != NULL)
+    {
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        slow = slow->next;
+        fast = fast->next->next;
 
-		/* If slow and fast meet, it means there is a loop */
-		if (slow == fast)
-		{
-			/* Print the looped node */
-			printf("[%p] %d\n", (void *)slow, slow->n);
-			return (count + 1);
-		}
-		count++;
-	}
+        /* If slow and fast meet, it means there is a loop */
+        if (slow == fast)
+        {
+            loop_detected = 1;
+            break;
+        }
+        count++;
+    }
 
-	/* Print the last node */
-	if (slow != NULL)
-	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		count++;
-	}
+    if (loop_detected)
+    {
+        /* Count the nodes in the loop */
+        size_t loop_count = 1;
+        const listint_t *temp = slow->next;
+        while (temp != slow)
+        {
+            loop_count++;
+            temp = temp->next;
+        }
 
-	return (count);
+        /* Print the looped node only once */
+        printf("[%p] %d\n", (void *)slow, slow->n);
+
+        /* Adjust the count to exclude the loop */
+        count += loop_count;
+    }
+
+    /* Print the remaining nodes after the loop */
+    while (slow != NULL)
+    {
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        slow = slow->next;
+        count++;
+    }
+
+    return count;
 }
